db48df8fbbac7696350a3b4c911b52e209256171
Quang Tuan NGO <quangtuanngo@microsoft.com>
2024-05-22 00:50:37 -0700

    - Implement incoming call

--- CallKitDemo/AppDelegate.swift
+++ CallKitDemo/AppDelegate.swift
@@ -9,12 +9,31 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+    
+    let callManager = CallManager()
+    var providerDelegate: ProviderDelegate!
+    
+    class var shared: AppDelegate {
+        return UIApplication.shared.delegate as! AppDelegate
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        providerDelegate = ProviderDelegate(callManager: callManager)
         return true
+    }
+    
+    func displayIncomingCall(
+        uuid: UUID,
+        handle: String,
+        hasVideo: Bool = false,
+        completion: ((Error?) -> Void)?
+    ) {
+        providerDelegate.reportIncomingCall(
+            uuid: uuid,
+            handle: handle,
+            hasVideo: hasVideo,
+            completion: completion)
     }
 
     // MARK: UISceneSession Lifecycle
